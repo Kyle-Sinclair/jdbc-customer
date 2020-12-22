cd525ac6c699ec2551e647fdd988f9af8598d278
Kyle Sinclair <Kyle.dean.sinclair@gmail.com>
2020-12-22 14:05:52 +0100

    Initial commit

---
+++ src/main/java/com/example/jdbccustomer/JdbcCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbccustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcCustomerApplication.class, args);
+    }
+
+}
